9c8cb8a8b514736e97d50914963b4aed9387763f
Deluxateer <deluxateer@gmail.com>
2020-01-09 15:26:19 -0600

    add basic conversion endpoints (get calc, post calc)

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/index.html`)
+})
+
+app.post('/', function(req, res) {
+  const baseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/'
+  const { crypto, fiat } = req.body;
+  const requestURL = baseURL + crypto + fiat
+
+  request(requestURL, function(error, response, body) {
+    const data = JSON.parse(body)
+    const price = data.last
+    
+    res.send(`<h1>The price of ${crypto} is currently ${price} ${fiat}.</h1>`)
+  })
 })
 
 const port = 3000
